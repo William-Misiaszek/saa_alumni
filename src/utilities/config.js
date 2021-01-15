a4fbd0d763ddb6f883844e0a2db97f47ff685adf
Yvonne Tang <ytang2@stanford.edu>
2021-01-14 18:04:31 -0800

    Add config file

---
+++ src/utilities/config.js
@@ -0,0 +1,8 @@
+// Global variables for this project.
+
+const config = {
+  isNetlify: process.env.GATSBY_NETLIFY ?? false,
+  basePath: process.env.GATSBY_BASE_PATH === undefined ? '/' : process.env.GATSBY_BASE_PATH,
+};
+
+export { config };

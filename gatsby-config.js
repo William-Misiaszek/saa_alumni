a84ec09c74a3ead6139998102d83a66d135cfbc1
Yvonne Tang <ytang2@stanford.edu>
2021-01-14 17:27:35 -0800

    Add .env files; update gatsby-config and add gatsby anchor links plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,45 @@
+const path = require('path');
+
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: `Make a Gift`,
+    description: `Make a Gift to Stanford`,
+    author: `Stanford University Office of Development`,
+    siteUrl: ``,
   },
   plugins: [
+    `gatsby-plugin-anchor-links`,
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-storyblok',
+      resolve: 'gatsby-plugin-robots-txt',
       options: {
-        accessToken: 'bJQb8KcUXW4NJ35XJFwGuwtt',
-        homeSlug: 'home',
-        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft'
+        policy: [
+          { userAgent: '*', allow: '/' },
+          { userAgent: '*', disallow: '/editor/' }
+        ]
       }
     },
-    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [
+          `/editor/*`,
+          `/editor/`,
+          `/editor`,
+          `/global-components/*`,
+          `/global-components`,
+        ],
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -20,7 +48,27 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-transformer-sharp`
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: process.env.GATSBY_STORYBLOK_ACCESS_TOKEN,
+        homeSlug: 'home',
+        resolveRelations: [],
+        version: process.env.NODE_ENV == 'production' ? 'published' : 'draft'  // show only published on the front end site
+        // version: 'draft'  // would show any including drafts
+      }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Make a Gift`,
+        start_url: `/`,
+        include_favicon: false,
+        crossOrigin: `use-credentials`,
+        icons: [],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }

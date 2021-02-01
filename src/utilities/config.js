2f030f8554c471ae967bfd4ed3f7b214bca2d7f4
Yvonne Tang <ytang2@stanford.edu>
2021-02-01 09:38:10 -0800

    Small changes (#2)
    Co-authored-by: Sherakama <sherakama@gmail.com>

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -3,6 +3,7 @@
 const config = {
   isNetlify: process.env.GATSBY_NETLIFY ?? false,
   basePath: process.env.GATSBY_BASE_PATH === undefined ? '/' : process.env.GATSBY_BASE_PATH,
+  assetCdn: process.env.GATSBY_ASSET_CDN ?? "https://assets.stanford.edu/",
 };
 
 export { config };

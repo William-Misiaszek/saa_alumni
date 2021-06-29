23702dc2c39bd81c43d6d0e870f03247461ef0ff
Sherakama <sheamck@stanford.edu>
2021-06-29 09:10:49 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         accessToken: process.env.GATSBY_STORYBLOK_ACCESS_TOKEN,
         homeSlug: "home",
         resolveRelations: storyblokRelations,
-        version: "published",
+        process.env.NODE_ENV === "production" ? "published" : "draft",
         // version: 'draft'  // would show any including drafts
       },
     },

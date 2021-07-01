35563bc3c1f325912397925c9cf0c2ef46335d8d
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2021-07-01 12:10:57 -0700

    ADAPT-2661 Add mobile button padding (#57)
    * ADAPT-2661 Add mobile button padding
    
    * ADAPT-2661 Update button padding
    
    * ADAPT-2661 Adjust Button font size
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>
    
    * ADAPT-2661 Adjust Button font size
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>

--- src/utilities/dataSource.js
+++ src/utilities/dataSource.js
@@ -18,11 +18,11 @@ export const bgPositionVertical = {
 
 export const buttonSizes = {
   small:
-    "su-px-20 su-pt-10 su-pb-12 md:su-px-26 md:su-pt-14 md:su-pb-16 su-text-18 md:su-text-20",
+    "su-px-20 su-pt-10 su-pb-11 sm:su-px-20 sm:su-pt-10 sm:su-pb-12 md:su-px-26 md:su-pt-14 md:su-pb-16 su-text-18 md:su-text-20",
   default:
-    "su-px-20 su-pt-14 su-pb-16 md:su-px-30 md:su-pt-16 md:su-pb-18 su-text-20 md:su-text-24",
+    "su-px-20 su-pt-10 su-pb-11 sm:su-px-20 sm:su-pt-14 sm:su-pb-16 md:su-px-30 md:su-pt-16 md:su-pb-18 su-text-18 md:su-text-24",
   large:
-    "su-px-26 su-pt-16 su-pb-18 md:su-px-36 md:su-pt-[22px] md:su-pb-[24px] su-text-20 md:su-text-24",
+    "su-px-20 su-pt-10 su-pb-11 sm:su-px-26 sm:su-pt-16 sm:su-pb-18 md:su-px-36 md:su-pt-[22px] md:su-pb-[24px] su-text-18 md:su-text-24",
 };
 
 export const buttonStyles = {

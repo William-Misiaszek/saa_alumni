5caf858ecce2438883d14d17bb6608096931b287
Matt Anglin <mattanglin@gmail.com>
2021-10-18 13:32:36 -0700

    fix siteUrl resolution

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,10 +23,21 @@ const storyblokRelations = [
   'tripCard.trip',
 ];
 
-const siteUrl =
-  process.env.CONTEXT === 'production'
-    ? process.env.URL
-    : process.env.DEPLOY_PRIME_URL || 'http://localhost:8000';
+// Support for Gatsby CLI
+let siteUrl = 'http://localhost:8000';
+
+// Support for Production site builds.
+if (process.env.CONTEXT === 'production') {
+  siteUrl = process.env.URL;
+}
+// Support for non-production netlify builds (branch/preview)
+else if (process.env.CONTEXT !== 'production' && process.env.NETLIFY) {
+  siteUrl = process.env.DEPLOY_PRIME_URL;
+}
+// Support for Netlify CLI.
+else if (process.env.NETLIFY_DEV === true) {
+  siteUrl = 'http://localhost:64946';
+}
 
 module.exports = {
   siteMetadata: {

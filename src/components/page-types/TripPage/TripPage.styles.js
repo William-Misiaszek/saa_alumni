ecb8df3f9b63e17aaa9a716b8826d05428ca8bf4
Yvonne Tang <ytang2@stanford.edu>
2022-01-06 17:47:52 -0800

    lint

--- src/components/page-types/TripPage/TripPage.styles.js
+++ src/components/page-types/TripPage/TripPage.styles.js
@@ -1,3 +1,2 @@
-export const main =
-  'su-relative su-flex-grow su-w-full su-basefont-23';
+export const main = 'su-relative su-flex-grow su-w-full su-basefont-23';
 export const startAlert = 'su-mb-0 su-leading-display su-card-paragraph';

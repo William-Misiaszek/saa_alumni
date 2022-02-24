6e2b264ee35cf456e8bb7e922c783547525bccba
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2022-02-24 15:16:14 -0800

    ADAPT-4435 | @rebeccahongsf | clone gateway auth (#299)
    * ADAPT-4388: Update gatsby netlify plugin
    
    * clone api gateway auth
    
    * tweak return token
    
    * tweak comment
    
    * move ApiGatewayAuth into utilities and remove directory from api folder
    
    * add back in gateway auth test page
    
    * add back in header for testing
    
    * update err response
    
    * remove setHeader
    
    * remove gateway test page
    
    Co-authored-by: Elena <elena.cracea@gmail.com>

---
+++ src/utilities/ApiGatewayAuth.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export class ApiGatewayAuth {
+  constructor(config = {}) {
+    this.url = config.url || process.env.API_GATEWAY_AUTH_URL;
+    this.clientId = config.clientId || process.env.API_GATEWAY_AUTH_CLIENT_ID;
+    this.clientSecret =
+      config.clientSecret || process.env.API_GATEWAY_AUTH_CLIENT_SECRET;
+    this.token = config.token;
+    // TODO: Configurable?
+    this.grantType = 'CLIENT_CREDENTIALS';
+  }
+
+  isAuthenticated = () => !!this.token;
+
+  authenticate = async () => {
+    const params = {
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      grant_type: this.grantType,
+    };
+    const result = await axios.post(this.url, undefined, {
+      params,
+    });
+    this.token = result.data;
+
+    return this.token;
+  };
+}

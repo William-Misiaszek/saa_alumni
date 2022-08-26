b2aae8a2179d97102d1b903b4f8e487cb39dad51
Elena Samples <elena.cracea@gmail.com>
2022-08-26 14:28:48 -0600

    ADAPT-3917: Add mobule only menu items (#443)

--- src/components/navigation/MainNav/mainNavItem.jsx
+++ src/components/navigation/MainNav/mainNavItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { dcnb } from 'cnbuilder';
 import SbLink from '../../../utilities/sbLink';
 import * as styles from './mainNavitem.styles';
 
 // 2nd level Nav Items for SAA shared and Homesite Main Navs
-const MainNavItem = ({ blok: { link, text }, isHomesite }) => (
-  <li className={styles.root}>
+const MainNavItem = ({ blok: { link, text, mobileOnly }, isHomesite }) => (
+  <li className={dcnb(styles.root, mobileOnly ? 'lg:su-hidden' : '')}>
     <SbLink
       link={link}
       classes={styles.link({ isHomesite })}

9cc1e02df9c43d66339b53368b5f92d12bfbd6b8
Sherakama <sheamck@stanford.edu>
2022-09-02 16:32:42 -0700

    ADAPT-5247: Handle login error more gracefully. (#455)

--- src/api/auth/profile.js
+++ src/api/auth/profile.js
@@ -5,19 +5,18 @@
 import connect from 'next-connect';
 import { MegaProfile } from '../../utilities/MegaProfile';
 import { authInstance } from '../../utilities/authInstance';
-import { ExceptionHandler } from '../../utilities/ApiExceptions';
 import { fullggMockData } from '../../utilities/mocks';
 import { isStoryblokEditor } from '../../utilities/isStoryblokEditor';
 
 /**
  * Fetches the profile data from the MEGA PROFILE API endpoints.
  */
-const megaprofileHandler = async (req, res) => {
+const megaprofileHandler = async (req, res, next) => {
   const mp = new MegaProfile();
   const profileId = req.user.encodedSUID;
   const session = req.user;
   let fullgg = {};
-  let affiliations = {};
+  let affiliations = [];
 
   // While the authentication is between states support fetching by both oauth
   // services for the majority of the profile information.
@@ -25,19 +24,22 @@ const megaprofileHandler = async (req, res) => {
     const fullggresult = await mp.get(`/${profileId}/profiles/fullgg`);
     fullgg = fullggresult.data;
   } catch (err) {
-    console.error(ExceptionHandler(res, err));
+    console.error(err);
+    fullgg.name = {};
+    fullgg.name.digitalName = `${req.user.firstName} ${req.user.lastName}`;
   }
 
-  // Affiliations is already on the keycloak ouath so we fetch here.
+  // // Affiliations is already on the keycloak ouath so we fetch here.
   try {
     const mpresult = await mp.get(`/${profileId}/profiles/affiliations`);
     affiliations = mpresult.data.affiliations;
   } catch (err) {
-    console.error(ExceptionHandler(res, err));
+    console.error(err);
   }
 
   const mpUser = { session, ...fullgg, affiliations };
-  return res.status(200).json(mpUser);
+  res.status(200).json(mpUser);
+  next();
 };
 
 const storyblokPreviewPassthrough = async (req, res, next) => {

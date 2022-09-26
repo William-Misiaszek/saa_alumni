1ed7c2281cfe8d6c3a64aefb3dc241db46da27ab
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2022-09-26 12:26:16 -0700

    ADAPT-5438 | @rebeccahongsf | resolve scroll bug on chrome windows os (#476)

--- src/components/embed/dynaScript.js
+++ src/components/embed/dynaScript.js
@@ -16,8 +16,8 @@ const DynaScript = ({ errorBlok, src, id, ...props }) => {
 
   // When the component mounts load the script.
   useEffect(() => {
-    const showForm = () => {
-      setDisplay(true);
+    const showForm = (isDisplay) => {
+      setDisplay(isDisplay);
     };
 
     const scrollTop = () => {
@@ -32,7 +32,7 @@ const DynaScript = ({ errorBlok, src, id, ...props }) => {
         setScriptLoaded(true);
 
         // Once GiveGab form has completed rendering, display form
-        script.addEventListener('widgetRenderEnd', showForm);
+        script.addEventListener('widgetRenderEnd', showForm(true));
         // Once GiveGab form has been prompted to the next form page, bring user back to the top of the form
         script.addEventListener('widgetPageChange', scrollTop);
         // Once GiveGab form has successfully submitted, bring user back to the top of the form
@@ -47,7 +47,7 @@ const DynaScript = ({ errorBlok, src, id, ...props }) => {
 
     return () => {
       mounted = false;
-      script.removeEventListener('widgetRenderEnd', showForm);
+      script.removeEventListener('widgetRenderEnd', showForm(false));
       script.removeEventListener('widgetPageChange', scrollTop);
       script.removeEventListener('widgetComplete', scrollTop);
     };

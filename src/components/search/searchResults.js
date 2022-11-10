9102ea6fd041d4ab8fa0ed3bd169c171110c492f
jdwjdwjdw <jacobdeanwilson@gmail.com>
2022-11-10 09:30:02 -0800

    Filter results to remove forms with empty title

--- src/components/search/searchResults.js
+++ src/components/search/searchResults.js
@@ -22,6 +22,9 @@ const SearchResults = ({ results }) => {
     return <div />;
   }
 
+  // filter out form results with empty title
+  const filteredResults = results.hits.filter((result) => result.title !== '');
+
   return (
     <div id="search-results">
       <div
@@ -32,29 +35,30 @@ const SearchResults = ({ results }) => {
         tabIndex={0}
         id="number-search-results"
       >
-        <span className="su-font-semibold">{results.nbHits}</span> results:
+        <span className="su-font-semibold">{filteredResults.length}</span>{' '}
+        results:
       </div>
-      {results.hits.map((result) => (
+      {filteredResults.map((filteredResult) => (
         <div
-          key={result.objectID}
+          key={filteredResult.objectID}
           className="su-px-0 su-rs-py-2 md:su-rs-px-2 su-border-b su-border-black-40"
         >
           <div className="su-flex su-flex-wrap md:su-flex-nowrap">
             <div className="md:su-flex-1 su-w-full">
-              <div className="su-text-16 su-mb-10">{result.domain}</div>
+              <div className="su-text-16 su-mb-10">{filteredResult.domain}</div>
               <Heading level={3} size={1} font="serif">
                 <a
                   className="su-text-digital-red-light su-group su-transition-colors hocus:su-underline"
-                  href={checkParams(result.url)}
+                  href={checkParams(filteredResult.url)}
                 >
-                  {result.fileType === 'video' && (
+                  {filteredResult.fileType === 'video' && (
                     <HeroIcon
                       iconType="video"
                       className="su-inline-block su-mr-02em"
                       srText="Video: "
                     />
                   )}
-                  {result.fileType === 'audio' && (
+                  {filteredResult.fileType === 'audio' && (
                     <HeroIcon
                       iconType="podcast"
                       className="su-inline-block su-ml-01em"
@@ -65,19 +69,21 @@ const SearchResults = ({ results }) => {
                     // eslint-disable-next-line react/no-danger
                     dangerouslySetInnerHTML={{
                       // eslint-disable-next-line no-underscore-dangle
-                      __html: sanitize(result.title, { decodeEntities: false }),
+                      __html: sanitize(filteredResult.title, {
+                        decodeEntities: false,
+                      }),
                     }}
                   />
                   <HeroIcon
                     iconType={
-                      result.domain.match(/^alumni.stanford.edu/)
+                      filteredResult.domain.match(/^alumni.stanford.edu/)
                         ? 'arrow-right'
                         : 'external'
                     }
                     className="su-inline-block group-hover:su-text-cardinal-red group-focus:su-text-cardinal-red"
                     isAnimate
                     srText={
-                      result.domain.match(/^alumni.stanford.edu/)
+                      filteredResult.domain.match(/^alumni.stanford.edu/)
                         ? ''
                         : ' (external link)'
                     }
@@ -85,23 +91,23 @@ const SearchResults = ({ results }) => {
                 </a>
               </Heading>
               {/* eslint-disable-next-line no-underscore-dangle */}
-              {result._snippetResult?.body.value && (
+              {filteredResult._snippetResult?.body.value && (
                 <p
                   className="su-card-paragraph su-leading-snug su-mb-0"
                   // eslint-disable-next-line react/no-danger
                   dangerouslySetInnerHTML={{
                     // eslint-disable-next-line no-underscore-dangle
-                    __html: sanitize(result._snippetResult.body.value),
+                    __html: sanitize(filteredResult._snippetResult.body.value),
                   }}
                 />
               )}
             </div>
-            {result.image && (
+            {filteredResult.image && (
               <div className="su-rs-mt-0 su-w-[15rem] su-h-[10rem] md:su-w-[22.5rem] md:su-h-[15rem] md:su-ml-30">
                 <img
                   className="su-block su-object-cover su-object-center su-h-full su-w-full"
-                  src={result.image}
-                  alt={result.title}
+                  src={filteredResult.image}
+                  alt={filteredResult.title}
                   loading="lazy"
                 />
               </div>

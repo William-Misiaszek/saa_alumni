66ab0d96dbe0a668368279a92740e2c79f3b1b92
Elena Samples <elena.cracea@gmail.com>
2023-03-21 15:59:41 -0600

    ADAPT-5573 | @Lyafel | Inject SUID for the for embed (#607)
    * ADAPT-5573: Inject SUID for the for membed
    
    * ADAPT-5573: Replave encoded Suid with SUID

--- src/components/cards/embedCard.js
+++ src/components/cards/embedCard.js
@@ -12,15 +12,17 @@
  * is also not good practice to inject and manipulate the page outside of
  * REACT as that can lead to irregularities and troubles.
  */
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useContext } from 'react';
 import SbEditable from 'storyblok-react';
 import postscribe from 'postscribe';
 import ClipLoader from 'react-spinners/ClipLoader';
+import AuthContext from '../../contexts/AuthContext';
 
-const EmbedCard = ({ blok: { embed: html }, blok }) => {
+const EmbedCard = ({ blok: { embed: html, injectSuid }, blok }) => {
   const myEmbed = useRef(null);
   const uniqueId = `su-alumni-${blok._uid}`;
   const [scriptLoaded, setScriptLoaded] = useState(false);
+  const { userProfile } = useContext(AuthContext);
 
   useEffect(() => {
     if (!html) return;
@@ -35,6 +37,9 @@ const EmbedCard = ({ blok: { embed: html }, blok }) => {
       // Create a 'tiny' document and parse the html string.
       // https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
       const miniDom = document.createRange().createContextualFragment(html);
+      if (injectSuid && miniDom.childNodes[0].src) {
+        miniDom.childNodes[0].src = `${miniDom.childNodes[0].src}?suid=${userProfile.session.SUID}`;
+      }
       // Clear the container.
       myEmbed.current.innerHTML = '';
       // Append the new content.
